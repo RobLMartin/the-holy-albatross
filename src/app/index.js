f7966e45a915d0a6d8f0907b4206307150e7788d
Rob Martin <rmartin3@alumni.unca.edu>
2020-08-15 16:32:16 -0700

    initial commit

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../logo.svg";
+import "./App.css";
+import HolyAlbatross from "../holy-albatross";
+
+const App = () => (
+  <>
+    <p>Hello World!</p>
+    <HolyAlbatross />
+  </>
+);
+
+export default App;
---
+++ src/holy-albatross/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const HolyAlbatross = () => <p>holy albatross</p>;
+export default HolyAlbatross;
