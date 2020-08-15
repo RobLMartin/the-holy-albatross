438fa5f630d6f5b5d15d4a3206fe5a5491925758
Rob Martin <rmartin3@alumni.unca.edu>
2020-08-15 16:49:09 -0700

    base jsx

--- src/app/index.js
+++ src/app/index.js
@@ -3,7 +3,47 @@ import "./App.css";
 
 const App = () => (
   <>
-    <p>Hello World!</p>
+    <header>
+      <div className="container flow centered">
+        <h1 className="title">The Holy Albatross</h1>
+      </div>
+    </header>
+    <main class="main-content">
+      <div className="container">
+        <div className="columns">
+          <div className="card flow">
+            <h3 className="card__title">Card 1</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
+              sed, ratione provident veniam eius enim reiciendis, alias rerum
+              animi distinctio molestiae perspiciatis? Ducimus soluta totam
+              sapiente in! Perferendis vero ratione inventore reiciendis sunt
+              quis labore animi sint error fuga!
+            </p>
+          </div>
+          <div className="card flow">
+            <h3 className="card__title">Card 2</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
+              molestiae!
+            </p>
+          </div>
+          <div className="card flow">
+            <h3 className="card__title">Card 3</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint
+              provident fugit quod recusandae molestias est consequatur sed,
+              laudantium commodi temporibus.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="width-indicator">
+        <p>
+          The width of <code>.columns</code> is:
+        </p>
+      </div>
+    </main>
   </>
 );
 
