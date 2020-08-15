6455685ff35bd38133cdc9b2d9730f168917cda4
Rob Martin <rmartin3@alumni.unca.edu>
2020-08-15 16:37:32 -0700

    remove unneeded files.

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,9 @@
 import React from "react";
-import logo from "../logo.svg";
 import "./App.css";
-import HolyAlbatross from "../holy-albatross";
 
 const App = () => (
   <>
     <p>Hello World!</p>
-    <HolyAlbatross />
   </>
 );
 
--- src/holy-albatross/index.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-
-const HolyAlbatross = () => <p>holy albatross</p>;
-export default HolyAlbatross;
